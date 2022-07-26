1ae0eb869bf4c5b75d08871656c2eab83fbb3042
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-26 08:02:42 +0100

    Renaming the file name and remove all the numbers

--- Rename Files Program/rename_files.py
+++ Rename Files Program/rename_files.py
@@ -3,7 +3,15 @@ import os
 def rename_files():
     #(1) get file names from a folder
     file_list = os.listdir(r"C:\prank")
-    print(file_list)
+    #print(file_list)
+    saved_path = os.getcwd()
+    print("Current Working Directory is " + saved_path)
+    os.chdir(r"C:\prank")
     #(2) for each file, rename filename
+    for file_name in file_list:
+        print("Old Name: " + file_name)
+        print("New Name: " + ''.join(i for i in file_name if not i.isnumeric()))
+        os.rename(file_name, ''.join(i for i in file_name if not i.isnumeric()) )
+    os.chdir(saved_path)
 
 rename_files()
